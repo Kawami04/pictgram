bd2c8dc3c1ef304e272981a0a3620c10ff91693b
kawamura <takumi.04kawamura@gmail.com>
2019-03-28 21:26:17 +0900

    rails05kadai

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 15 }
+  validates :email, presence: true, format: {with: /\w+@\w+\.\w+/}
+  validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}\z/i}
+
+  has_secure_password
 end
