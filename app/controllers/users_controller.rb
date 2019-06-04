2e71cdac37d5493e0993dfe89ed76fa646ba0e74
kawamura <takumi.04kawamura@gmail.com>
2019-06-05 02:26:37 +0900

    rails07

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,10 +6,10 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-    redirect_to root_path, success: '登録が完了しました'
+      redirect_to root_path, success: '登録が完了しました'
     else
-     flash.now[:danger] = "登録が失敗しました"
-    render :new
+      flash.now[:danger] = "登録が失敗しました"
+      render :new
     end
   end
 
